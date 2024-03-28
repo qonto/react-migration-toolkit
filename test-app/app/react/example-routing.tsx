ffdcb693c980caf20da8c9068f464a3aff8703a4
Paul Gaumer <paul.gaumer@qonto.com>
2024-03-28 17:31:26 +0100

    integrate router provider & Link

---
+++ test-app/app/react/example-routing.tsx
@@ -0,0 +1,50 @@
+import { Link } from "react-migration-toolkit/react/components/link";
+import { useRouter } from "react-migration-toolkit/react/hooks/use-router";
+
+export function ExampleRouting() {
+  const router = useRouter();
+
+  return (
+    <div>
+      <Link data-test-about-link href="/about">
+        Visit About page
+      </Link>
+
+      <button
+        data-test-about-button-push
+        onClick={() => {
+          router.push("/about");
+        }}
+      >
+        Push to About page
+      </button>
+
+      <button
+        data-test-about-button-push-query
+        onClick={() => {
+          router.push("/about?foo=bar&baz=qux");
+        }}
+      >
+        Push to About page with query params
+      </button>
+
+      <button
+        data-test-about-button-replace
+        onClick={() => {
+          router.replace("/about");
+        }}
+      >
+        Replace to About page
+      </button>
+
+      <button
+        data-test-about-button-replace-query
+        onClick={() => {
+          router.replace("/about?foo=bar&baz=qux");
+        }}
+      >
+        Replace to About page with query params
+      </button>
+    </div>
+  );
+}
