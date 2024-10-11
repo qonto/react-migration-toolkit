7a1dba0ab95fafcdc4abecd5fa586925da861c5a
Wandrille VERLUT <wandrille.verlut@qonto.com>
2024-10-11 17:50:43 +0200

    feat( react bridge): handle safe strings

--- test-app/app/components/example.js
+++ test-app/app/components/example.js
@@ -1,6 +1,15 @@
 import Component from '@glimmer/component';
 import { Example as ReactExample } from '../react/example';
+import { htmlSafe } from '@ember/template';
+
+let safeText = htmlSafe(
+  'Click on my  <a href="https://www.google.com">link</a>',
+);
 
 export default class Example extends Component {
   reactExample = ReactExample;
+
+  get safeText() {
+    return safeText;
+  }
 }
