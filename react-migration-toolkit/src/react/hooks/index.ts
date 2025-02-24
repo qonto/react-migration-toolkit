edc95bb58b801fda31bbc58c48fa9d739cdabb76
Antoine Moues <antoine.moues@qonto.com>
2025-02-24 10:24:34 +0100

    feat(PolymorphicNavigateProvider): add provider for useNavigate

--- react-migration-toolkit/src/react/hooks/index.ts
+++ react-migration-toolkit/src/react/hooks/index.ts
@@ -1,4 +1,5 @@
 export { useEmberService } from './use-ember-service';
 export { useApplicationInstance } from './use-application-instance';
 export { useFlags } from './use-flags';
+export { useNavigate } from './use-navigate';
 export { useRouter } from './use-router';
