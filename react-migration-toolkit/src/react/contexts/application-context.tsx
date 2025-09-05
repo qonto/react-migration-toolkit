722676503d1e08ad02e0fc2833cc32e6bb4e67dc
Paweł Bator <720354+jembezmamy@users.noreply.github.com>
2025-09-05 15:08:30 +0200

    fix: hide app context when owner is destroyed

--- react-migration-toolkit/src/react/contexts/application-context.tsx
+++ react-migration-toolkit/src/react/contexts/application-context.tsx
@@ -1,4 +1,17 @@
-import { createContext, type ReactNode, type PropsWithChildren } from 'react';
+import {
+  createContext,
+  type ReactNode,
+  type PropsWithChildren,
+  useState,
+  useEffect,
+} from 'react';
+import {
+  createContext,
+  type ReactNode,
+  type PropsWithChildren,
+  useState,
+  useEffect,
+} from 'react';
 import type ApplicationInstance from '@ember/application/instance';
 
 interface ApplicationProviderProps extends PropsWithChildren {
@@ -13,6 +26,20 @@ export function ApplicationProvider({
   owner,
   children,
 }: ApplicationProviderProps): ReactNode {
+  const [isDestroyed, setIsDestroyed] = useState<boolean>(false);
+
+  useEffect(() => {
+    const oldWillDestroy = owner.willDestroy;
+    owner.willDestroy = (...args) => {
+      setIsDestroyed(true);
+      oldWillDestroy.apply(owner, args);
+    };
+  }, [owner]);
+
+  if (isDestroyed) {
+    return null;
+  }
+
   return (
     <ApplicationContext.Provider value={owner}>
       {children}
